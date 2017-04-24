51fec4943b7d6b88f55b44ec59025d684a2c9fab
Bruno <mondelob14@gmail.com>
2017-04-24 09:18:17 +0200

    Added source directory

---
+++ src/unittree.c
@@ -0,0 +1,18 @@
+/*
+ * Shows all the sub-targets from a unit
+*/
+
+#include <stdio.h> 
+#include <stdlib.h> // System Variables
+#include <getopt.h> //Argparse
+
+#define PROGRAM_NAME "unittree"
+
+#define AUTHORS \
+    proper_name("Bruno Mondelo")
+    
+int main (int argc, char argv[]) {
+    
+    printf("Hello World!\n");
+    
+}
